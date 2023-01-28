20032cae78381a6dcb17cd3e69f2c15657e3f27b
Алексей Чайкин <barjastroy@gmail.com>
2023-01-28 15:21:56 +0300

    init project

---
+++ CarCost/CarCost/CarCostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCostApp.swift
+//  CarCost
+//
+//  Created by macbook on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarCostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCost/CarCost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarCost
+//
+//  Created by macbook on 28.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
