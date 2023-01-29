f0d11f8a6291b53d0327a5c7c3ccb10d19b9652d
Алексей Чайкин <barjastroy@gmail.com>
2023-01-29 17:28:03 +0300

    Create StatisticView.swift

---
+++ CarCost/CarCost/StatisticView.swift
@@ -0,0 +1,21 @@
+//
+//  StatisticView.swift
+//  CarCost
+//
+//  Created by macbook on 29.01.2023.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text("Statistic")
+            
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView()
+    }
+}
